083bc9c489a48d820b3a3440eabc42a7b59a213e
i-sunni <61475906+i-sunni@users.noreply.github.com>
2021-04-30 18:27:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,26 +6,25 @@ import "./Timer.js";
 export default function App() {
   return (
     <div className="App">
-      <Piece location="" color="" type="" is_active="" />
+      {/* <Piece location="" color="" type="" is_active="" /> */}
       <Chessboard />
-      <Chessboard />
-      # Nasir (Red)
-      <Piece location="A1" color="Red Pawn 1" />
-      <Piece location="A2" color="Red Pawn 2" />
-      <Piece location="A3" color="Red Pawn 3" />
-      <Piece location="A4" color="Red Pawn 4" />
-      <Piece location="A5" color="Red Pawn 5" />
-      <Piece location="A6" color="Red Pawn 6" />
-      <Piece location="A7" color="Red Pawn 7" />
-      <Piece location="A8" color="Red Pawn 8" />
-      <Piece location="B1" color="Red Rook 1" />
-      <Piece location="B2" color="Red Rook 2" />
-      <Piece location="B3" color="Red Knight 1" />
-      <Piece location="B4" color="Red Knight 2" />
-      <Piece location="B5" color="Red Bishop 1" />
-      <Piece location="B6" color="Red Bishop 2" />
-      <Piece location="B7" color="Red King" />
-      <Piece location="B8" color="Red Queen" />
+      {/* # Nasir (Red)
+      <Piece location="a1" color="Red Pawn 1" />
+      <Piece location="a2" color="Red Pawn 2" />
+      <Piece location="a3" color="Red Pawn 3" />
+      <Piece location="a4" color="Red Pawn 4" />
+      <Piece location="a5" color="Red Pawn 5" />
+      <Piece location="a6" color="Red Pawn 6" />
+      <Piece location="a7" color="Red Pawn 7" />
+      <Piece location="a8" color="Red Pawn 8" />
+      <Piece location="b1" color="Red Rook 1" />
+      <Piece location="b2" color="Red Rook 2" />
+      <Piece location="b3" color="Red Knight 1" />
+      <Piece location="b4" color="Red Knight 2" />
+      <Piece location="b5" color="Red Bishop 1" />
+      <Piece location="b6" color="Red Bishop 2" />
+      <Piece location="b7" color="Red King" />
+      <Piece location="b8" color="Red Queen" />
       # Denny (Blue)
       <Piece location="0" color="blue" type="pawn 1" />
       <Piece location="0" color="blue pawn 2" />
@@ -42,7 +41,8 @@ export default function App() {
       <Piece location="0" color="blue bishop 1" />
       <Piece location="0" color="blue bishop 2" />
       <Piece location="0" color="blue king" />
-      <Piece location="0" color="blue queen" />
+      <Piece location="0" color="blue queen" /> */}
+      
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
