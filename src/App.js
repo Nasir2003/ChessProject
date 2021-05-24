46c145a50592804bfaf9615bb4af2cdb7a5c3c6c
Nasir2003 <43577897+Nasir2003@users.noreply.github.com>
2021-05-24 17:59:05 -0400

    Still working on timer

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
 import "./styles.css"; 
 import Chessboard from "./Chessboard.js";
-import Piece from "./Piece.js";
-import "./Timer.js";
+import Timer from "./Timer.js";
 
 export default function App() {
   return (
     <div className="App">
-      {/* <Piece location="" color="" type="" is_active="" /> */}
       <Chessboard />
-      {/* # Nasir (Red)
-      <Piece location="a1" color="Red Pawn 1" />
-      <Piece location="a2" color="Red Pawn 2" />
-      <Piece location="a3" color="Red Pawn 3" />
-      <Piece location="a4" color="Red Pawn 4" />
-      <Piece location="a5" color="Red Pawn 5" />
-      <Piece location="a6" color="Red Pawn 6" />
-      <Piece location="a7" color="Red Pawn 7" />
-      <Piece location="a8" color="Red Pawn 8" />
-      <Piece location="b1" color="Red Rook 1" />
-      <Piece location="b2" color="Red Rook 2" />
-      <Piece location="b3" color="Red Knight 1" />
-      <Piece location="b4" color="Red Knight 2" />
-      <Piece location="b5" color="Red Bishop 1" />
-      <Piece location="b6" color="Red Bishop 2" />
-      <Piece location="b7" color="Red King" />
-      <Piece location="b8" color="Red Queen" />
-      # Denny (Blue)
-      <Piece location="0" color="blue" type="pawn 1" />
-      <Piece location="0" color="blue pawn 2" />
-      <Piece location="0" color="blue pawn 3" />
-      <Piece location="0" color="blue pawn 4" />
-      <Piece location="0" color="blue pawn 5" />
-      <Piece location="0" color="blue pawn 6" />
-      <Piece location="0" color="blue pawn 7" />
-      <Piece location="0" color="blue pawn 8" />
-      <Piece location="0" color="blue rook 1" />
-      <Piece location="0" color="blue rook 2" />
-      <Piece location="0" color="blue knight 1" />
-      <Piece location="0" color="blue knight 2" />
-      <Piece location="0" color="blue bishop 1" />
-      <Piece location="0" color="blue bishop 2" />
-      <Piece location="0" color="blue king" />
-      <Piece location="0" color="blue queen" /> */}
-      
-      
+      <Timer />
     </div>
   );
 }
---
+++ src/Timer.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function Timer(props) {
+
+  // TODO: Use setInterval to update some "timer"
+  // state variable every second.
+  
+  // let [i , seti] = useState(0);
+  // function myFunc(){
+  //   seti(i + 1);
+  //   console.log(i)
+  // }
+  // useEffect(() => {
+  //   setInterval(myFunc, 60000);
+  // });
+  return (
+    <div>
+      {/* <h1>Minutes Elapsed: {i}</h1> */}
+    </div>
+  );
+}
