1d182c585386dbad3a212e93fcae7f91c5016cea
Nasir2003 <43577897+Nasir2003@users.noreply.github.com>
2021-06-07 16:40:30 -0400

    Timer done

--- src/Timer.js
+++ src/Timer.js
@@ -1,21 +1,15 @@
 import { useEffect, useState } from "react";
 
 export default function Timer(props) {
+  let [i, seti] = useState(0);
+  useEffect(() => {
+    let myFunc = function () {
+      seti((i) => i + 1);
+    };
+    let id = setInterval(myFunc, 60000);
+    console.log("setInternal called");
+    return () => clearInterval(id);
+  }, []);
 
-  // TODO: Use setInterval to update some "timer"
-  // state variable every second.
-  
-  // let [i , seti] = useState(0);
-  // function myFunc(){
-  //   seti(i + 1);
-  //   console.log(i)
-  // }
-  // useEffect(() => {
-  //   setInterval(myFunc, 60000);
-  // });
-  return (
-    <div>
-      {/* <h1>Minutes Elapsed: {i}</h1> */}
-    </div>
-  );
+  return <div>{<h1>Minutes Elapsed: {i}</h1>}</div>;
 }
